a0cd8aae3d31b109396c5a4e58033e1ed40986ec
toptasc <toptascagri@gmail.com>
2024-03-05 19:42:54 +0000

    added jenkins server terraform files

---
+++ infrastructure/msp-9-jenkins-server-template/jenkins_variables.tf
@@ -0,0 +1,10 @@
+variable "cagriskey" {}
+variable "ami" {
+  description = "amazon linux 2023 ami"
+}
+variable "region" {}
+variable "instance_type" {}
+variable "jenkins_server_secgr" {}
+variable "jenkins-server-tag" {}
+variable "jenkins-profile" {}
+variable "jenkins-role" {}
